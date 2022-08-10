1a3d871f000de7f52b020b3756a33a26dfcaac39
Danielle <danielle@Danielles-MacBook-Air.local>
2022-08-10 16:41:53 -0400

    reworking HTML

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # Team Profile Generator
+Created by Danielle Locklear
 
 ## Description
 This project is command-line application that is ran through Node.js. Once initiated it requests information from the person using it about members of this team profile. The information requested is the name of members in the team along with their member ID, email address and another piece of information such as GitHub username, school or phone number. 
+
+## Tools
+Node.js for command-line activation. <br>
+Bootstrap for organization and css formatting. <br>
+Jest is used to test accurateness in Javascript. <br>
+
+## Generated HTML
--- src/templateHTML.js
+++ src/templateHTML.js
@@ -2,11 +2,11 @@ const generateManager = function (manager) {
     return `
     <div class="col-4 mt-4">
         <div class="card h-100">
-            <div class="card-header">
+            <div class="profile-header">
                 <h3>${manager.name}</h3>
                 <h4>Manager</h4><i class="material-icons">content_paste</i>
             </div>
-            <div class="card-body">
+            <div class="profile-body">
                 <p class="id">ID: ${manager.id}</p>
                 <p class="email">Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
                 <p class="office">Office Number: ${manager.officeNumber}</p>
@@ -21,11 +21,11 @@ const generateEngineer = function (engineer) {
     return `
     <div class="col-4 mt-4">
         <div class="card h-100">
-            <div class="card-header">
+            <div class="profile-header">
                 <h3>${engineer.name}</h3>
                 <h4>Engineer</h4><i class="material-icons">laptop_mac</i>
             </div>
-            <div class="card-body">
+            <div class="profile-body">
                 <p class="id">ID: ${engineer.id}</p>
                 <p class="email">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
                 <p class="github">Github: <a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
@@ -40,11 +40,11 @@ const generateIntern = function (intern) {
     return `
     <div class="col-4 mt-4">
         <div class="card h-100">
-            <div class="card-header">
+            <div class="profile-header">
                 <h3>${intern.name}</h3>
                 <h4>Intern</h4><i class="material-icons">assignment_ind</i>
             </div>
-            <div class="card-body">
+            <div class="profile-body">
                 <p class="id">ID: ${intern.id}</p>
                 <p class="email">Email:<a href="mailto:${intern.email}">${intern.email}</a></p>
                 <p class="school">School: ${intern.school}</p>
@@ -114,8 +114,8 @@ const generateTeamPage = function (employeeCards) {
   </head>
   <body>
       <header>
-          <nav class="navbar" id="navbar">
-              <span class="navbar-brand mb-0 h1 w-100 text-center" id="navbar-text">Team Profile</span>
+          <nav class="topbar" id="topbar">
+              <span class="topbar-brand mb-0 h1 w-100 text-center" id="topbar-text">Team Profile</span>
           </nav>
       </header>
       <main>
